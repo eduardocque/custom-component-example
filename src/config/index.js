be9d6df4c75951153b60c479fc8ae20f98b9fa9e
Carlos Rodrigues <carlos_eduardo309@hotmail.com>
2023-09-11 15:52:57 +1000

    clean up

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-const devProps = {
-  apiServerUrl: "http://localhost:6081",
-  serverUrl: "http://localhost:8888/graphql",
-  subscriptionServerUrl: "ws://localhost:8888/subscriptions",
-  runReduxDevTools: true
-};
-
-export { devProps };
